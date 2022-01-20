f5bae4c61c24b4b345fab1a57f3eeeb0c1204224
Pham Minh Duy <phamminhduy191199@gmail.com>
2022-01-20 15:16:40 +0700

    LAB06_2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import HeaderComponent from './components/HeaderComponent'
+import ContactComponent from './components/ContactComponent'
+// import HeaderComponent from './components/HeaderComponent'
 
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <HeaderComponent />
+        <ContactComponent />
+        {/* <HeaderComponent /> */}
       </div>
     )
   }
