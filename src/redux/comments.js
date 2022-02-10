67c048878cbbb878ca011855539f142e09264e3e
Pham Minh Duy <phamminhduy191199@gmail.com>
2022-02-10 17:10:45 +0700

    Combining Reducers

---
+++ src/redux/comments.js
@@ -0,0 +1,9 @@
+import { COMMENTS } from '../shares/comments';
+
+export const Comments = (state = COMMENTS, action) => {
+    switch (action.type) {
+
+        default:
+          return state;
+      }
+};
