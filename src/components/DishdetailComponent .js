e76207fc7e4735605828dac4284616c153825b85
Pham Minh Duy <phamminhduy191199@gmail.com>
2022-02-11 11:01:56 +0700

    Redux Actions

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -1,50 +1,30 @@
-// function DishDetail(props) {
-//     return(
-//         <div>
-//             {props.dish.map((dish) => {
-//                 return (
-//                     <>
-//                          <h1>Comments</h1>
-//                          {dish.comments.map(comment => {
-//                              return (
-//                                  <div key= {comment.id}>
-//                                  <p>{comment.comment}</p>
-//                                  <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-//                                  </div>
-//                              )
-//                          })}
-//                     </>
-
-
-//                 )
-//             })}
-//         </div>
-//     )
-// }
-
-// export default DishDetail
-
+import { useState } from 'react'
+import {
+    Container, Row, Label, FormGroup, Form, Input, Col, Button, FormFeedback, ModalBody, ModalFooter, ModalHeader, Modal, Breadcrumb, BreadcrumbItem
+} from 'reactstrap';
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
 function RenderDish({ dish }) {
-        const Dish = dish.map((dish) => {
-            return (
-                <>
-                    <h1>Comments</h1>
-                    <img src={dish.image} alt="dang tai xuong"/>
-                </>
-            )
-        })
-        return Dish
+    const Dish = dish.map((dish) => {
+        return (
+            <div key={dish.id}>
+                <h1>Comments</h1>
+                <img src={dish.image} alt="dang tai xuong" />
+            </div>
+        )
+    })
+    return Dish
 }
 
-function RenderComments({ comments }) {
-      
+function RenderComments({ comments, addComment, dishId }) {
+
     const Comments = comments.map(comment => {
         return (
             <div key={comment.id}>
@@ -53,35 +33,131 @@ function RenderComments({ comments }) {
             </div>
         )
     })
-    return Comments
+    return (
+        <div>
+            {Comments}
+            <CommentForm dishID = {dishId} addComment={addComment} />
+        </div>
+    )
+}
+
+function CommentForm(props) {
+    const [modal, setModal] = useState(false)
+    const handleSubmit = (value) => {
+        console.log(JSON.stringify(value))
+        props.addComment(props.dishID, value.rating, value.name, value.comment)
+        alert(value)
+        setModal(!modal)
+    }
+    return (
+        <div >
+            <Button onClick={() => setModal(!modal)}>
+                Submit Comment
+            </Button>
+            <Modal isOpen={modal} >
+                <ModalHeader >
+                    Thêm nhân viên mới
+                </ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(value) => handleSubmit(value)}>
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={2}>Rating</Label>
+                            <Col md={10}>
+                                <Control.text model=".rating" id="rating" name="rating"
+                                    validators={{ required, minLength: minLength(0), maxLength: maxLength(2) }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".rating"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 0 characters',
+                                        maxLength: 'Must be 2 characters or less'
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="name" md={2}>Your Name</Label>
+                            <Col md={10}>
+                                <Control.text model=".name" id="name" name="name"
+                                    placeholder="Name..."
+                                    className="form-control"
+                                    validators={{ required, minLength: minLength(5), maxLength: maxLength(15) }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".name"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 5 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Label htmlFor="comment" md={2}>Comment</Label>
+                            <Col md={10}>
+                                <Control.textarea model=".comment" id="comment" name="comment"
+                                    className="form-control"
+                                    validators={{ required, minLength: minLength(5), maxLength: maxLength(100) }}
+                                />
+                                <Errors
+                                    className="text-danger"
+                                    model=".comment"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 5 characters',
+                                        maxLength: 'Must be 100 characters or less'
+                                    }}
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="form-group">
+                            <Col md={{ size: 10, offset: 2 }}>
+                                <Button type="submit" color="primary">
+                                    Tạo mới
+                                </Button>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+        </div>
+    )
 }
 
 const DishDetail = (props) => {
-    console.log(props.dish)
-    console.log(props.comments)
-    return(
+    return (
         <div className="container">
-                <div className="row">
-                    <Breadcrumb>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish[0].id}
+                    />
+                </div>
 
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
-                    </div>
-                </div>
-                </div>
+            </div>
+        </div>
     )
-    }
+}
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent ';
-import { DISHES } from '../shares/dishes';
 import { Routes, Route, Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -10,9 +9,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { COMMENTS } from '../shares/comments';
-import { PROMOTIONS } from '../shares/promotions';
-import { LEADERS } from '../shares/leaders';
+import { addComment } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -24,17 +21,18 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
+
 
 class Main extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS
-    };
+
   }
 
 
@@ -42,9 +40,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
-          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     }
@@ -52,8 +50,10 @@ class Main extends Component {
     const DishWithId = ({ dishWithId }) => {
       console.log(dishWithId);
       return (
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === dishWithId)}
-          comments={this.state.comments.filter((comment) => comment.dishId === dishWithId)} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === dishWithId)}
+          comments={this.props.comments.filter((comment) => comment.dishId === dishWithId)}
+          addComment={this.props.addComment}
+           />
       );
     };
 
@@ -62,8 +62,8 @@ class Main extends Component {
         <Header />
         <Routes>
           <Route path='/home' element={<HomePage />} />
-          <Route path='/menu' element={<Menu dishes={this.state.dishes} />} />
-          {this.state.dishes.map((dish) => {
+          <Route path='/menu' element={<Menu dishes={this.props.dishes} />} />
+          {this.props.dishes.map((dish) => {
             return (
               <Route path={`/menu/${dish.id}`} element={<DishWithId dishWithId={dish.id} />} />
             )
@@ -78,4 +78,4 @@ class Main extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Main);
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
