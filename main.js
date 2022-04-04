6b33a425db1bbfb014cf9efcaa416b4073205c17
Pham Minh Duy <phamminhduy191199@gmail.com>
2022-04-04 15:54:52 +0700

    test_commmit

---
+++ main.js
@@ -0,0 +1,64 @@
+// // Object Contructor
+// function User(firstName, lastName, address) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.address = address;
+// }
+
+// var admin = new User('Duy', 'Pham', 'Quang Nam');
+// var user = new User('A', 'Nguyen', 'Quang Binh')
+
+// admin.age = 22;
+// user.age = 21;
+
+// console.log(admin);
+// console.log(user);
+
+// // Object propertype
+
+// User.prototype.school = "DHBK Da Nang";
+// User.prototype.getName = function() {
+//     return `${this.firstName} ${this.lastName}`;
+// }
+
+// console.log(admin.getName());
+// console.log(user.getName());
+
+// var User = [
+//     {
+//         name: 'John',
+//         id: 1
+//     },
+//     {
+//         name: 'Duy',
+//         id: 1
+//     },
+//     {
+//         name: 'Pham',
+//         id: 1
+//     },
+//     {
+//         name: 'Minh',
+//         id: 1
+//     },
+//     {
+//         name: 'Le',
+//         id: 1
+//     }
+    
+// ]
+
+// var result = User.find(function (User, in[0]dex) {
+//     console.log(index)
+//     return User.name == 'Le' ;
+// })
+
+// console.log(result)
+
+var DocumentElement = document.querySelector('.box');
+
+
+
+DocumentElement.innerHTML = '<h1>Javascript cơ bản</h1>';
+
+console.log(DocumentElement.classList.value)
